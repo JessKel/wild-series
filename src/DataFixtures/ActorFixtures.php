7e69ec05f1d7d66d5b7b491a27081f245bc2fe53
Jessica Keller <jessica.keller68@gmail.com>
2023-12-08 17:10:42 +0100

    quete18

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Actor;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class ActorFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+        
+        for ($i=0; $i < 10; $i++) {
+        $actor = new Actor();
+        $actor->setName($faker->firstName() . ' ' . $faker->lastName());
+        $actor->addProgram($this->getReference('program_' . ProgramFixtures::PROGRAMS[array_rand(ProgramFixtures::PROGRAMS)][0]));
+        
+        $manager->persist($actor);
+        $manager->flush();
+        }
+    }
+
+    public function getDependencies()
+    {
+        return [
+          ProgramFixtures::class,
+        ];
+    }
+
+
+}
